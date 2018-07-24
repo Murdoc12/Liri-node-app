2a4bd06bb59c03bd7fb82d728b293b7d6ad3893c
Murdoc12 <peelembcr2@gmail.comgit config --global user.email git config --global user.email peelembcr2@gmail.com>
2018-07-24 11:32:16 -0400

    node

---
+++ keys.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+exports.twitter = {
+  consumer_key: 'UCnjoDtNBsWB4u1jPp6hCOOAP',
+  consumer_secret:'	0LljyPzrqmeJuKm1mUeenfhzZJaKJV1gOAumxhMgcgZbvFMCig',
+  access_token_key: '	1038442532-U43ZnolCmPdUiQMws7BMFLEmjfD25KGic7MXx11',
+  access_token_secret: 'ASG047SNKdIGCK04im3Vxo3xaNMYDMRQnf7aUjN9FP4yK',
+};
+
+exports.spotify = {
+  id: 'bf520e96a5a4401e834b28c2422f28c7',
+  secret: 'c5e655e23e9342f2af0e1c75009d772f',
+};
