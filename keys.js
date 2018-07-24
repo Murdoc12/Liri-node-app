4a1274561d32332165e362c54aa905010fab0a64
Murdoc12 <peelembcr2@gmail.comgit config --global user.email git config --global user.email peelembcr2@gmail.com>
2018-07-24 17:59:38 -0400

    node

--- keys.js
+++ keys.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 
 exports.twitter = {
-  consumer_key: 'UCnjoDtNBsWB4u1jPp6hCOOAP',
-  consumer_secret:'	0LljyPzrqmeJuKm1mUeenfhzZJaKJV1gOAumxhMgcgZbvFMCig',
-  access_token_key: '	1038442532-U43ZnolCmPdUiQMws7BMFLEmjfD25KGic7MXx11',
+  consumer_key: 'SJ0rbnrDAwGZt9jDKfayqkFQI',
+  consumer_secret:'HtPFumhBMLZ1mGeLJQixVqrIKPE1trZJmuqLryTjqN4wrUIcQ3 ',
+  access_token_key: '1038442532-U43ZnolCmPdUiQMws7BMFLEmjfD25KGic7MXx11',
   access_token_secret: 'ASG047SNKdIGCK04im3Vxo3xaNMYDMRQnf7aUjN9FP4yK',
 };
 
